f504072a9cd82fc544b27c04e84aecaae0be97fd
alschmut <aschmutz1@web.de>
2019-06-06 17:13:36 +0100

    Refactor for possible addition of other fileExtensions

--- AJavaMain.py
+++ AJavaMain.py
@@ -5,16 +5,10 @@ from Java9ListenerExtended import Java9ListenerExtended
 import sys
 import os
 
-def get_file_content(filename):
-	try:
-		with open(filename, "r") as file:
-			file_content = file.read()
-			return file_content
-	except Exception as err:
-		print(f"Could not open file: {type(err)}: {err}")
-		return None
-	
-def parseFile(input_stream):
+def get_supported_extensions():
+	return ["java"]
+
+def parseJava9File(input_stream):
 	lexer = Java9Lexer(input_stream)
 	stream = CommonTokenStream(lexer)
 	parser = Java9Parser(stream)
@@ -24,20 +18,34 @@ def parseFile(input_stream):
 	walker.walk(listener, tree)
 	return listener.getIdentifiers()
 
+def parseFile(file_extension, input_stream):
+	if file_extension == "java":
+		return parseJava9File(input_stream)
+
+def get_file_content(filename):
+	try:
+		with open(filename, "r") as file:
+			file_content = file.read()
+			return file_content
+	except Exception as err:
+		print(f"Could not open file: {type(err)}: {err}")
+		return None
+
+def getFileExtension(filepath):
+	return filepath.split(".")[-1]
+
 def checkFileToParse(filepath):
-	if hasFileExtension(filepath, "java"):
+	file_extension = getFileExtension(filepath)
+	if file_extension in get_supported_extensions():
 		file_content = get_file_content(filepath)
 		if file_content:
 			input_stream = InputStream(file_content)
-			identifiers = parseFile(input_stream)
-			printIdentifier(identifiers)
-
+			identifiers = parseFile(file_extension, input_stream)
+			printIdentifier(identifiers)		
+			
 def printIdentifier(identifiers):
 	for identifier in sorted(identifiers, key=lambda k: k["type"]):
 		print(f'{identifier["type"]}: {identifier["name"]}')
-
-def hasFileExtension(file, extension):
-	return file.split(".")[-1] == extension
 
 def getFilePath(path, filename):
 	return path + os.sep + filename
@@ -49,7 +57,6 @@ def traverseDirectory(directory_path):
 			checkFileToParse(filepath)
 
 def main():
-	
 	if len(sys.argv) != 2:
 		print(f'[-] Usage: python {sys.argv[0]} <file_or_directory_path>')
 		test_dir = "/Users/alexandersch/Documents/Beruf/Master/7 Thesis/Antlr/java_test_dir/subdir1/subdir2/"
