be59a0f8296acd1aea8f09d2e7eb7cdc430e1a35
alschmut <aschmutz1@web.de>
2019-06-19 10:59:16 +0100

    Add support for Java7 and Java8

--- src/main/LanguageParser.py
+++ src/main/LanguageParser.py
@@ -3,6 +3,10 @@ from antlr4 import *
 from BaseListener import BaseListener
 from Language import Language
 
+from Java.JavaLexer import JavaLexer
+from Java.JavaParser import JavaParser
+from ExtendedListener.JavaParserListenerExtended import JavaParserListenerExtended
+
 from Java9.Java9Lexer import Java9Lexer
 from Java9.Java9Parser import Java9Parser
 from ExtendedListener.Java9ListenerExtended import Java9ListenerExtended
@@ -12,6 +16,20 @@ from Kotlin.KotlinParser import KotlinParser
 from ExtendedListener.KotlinParserListenerExtended import KotlinParserListenerExtended
 
 class LanguageParser(object):
+
+    def parse_java_file(self, input_stream):
+        lexer = JavaLexer(input_stream)
+        stream = CommonTokenStream(lexer)
+        parser = JavaParser(stream)
+        tree = parser.compilationUnit()
+        listener = JavaParserListenerExtended()
+
+        walker = ParseTreeWalker()
+        walker.walk(listener, tree)
+        keywords = LanguageParser.get_keywords(self, lexer)
+        identifier = listener.get_all_identifier()
+        return LanguageParser.combine_as_map(self, identifier, keywords)
+
     def parse_java9_file(self, input_stream):
         lexer = Java9Lexer(input_stream)
         stream = CommonTokenStream(lexer)
@@ -49,7 +67,7 @@ class LanguageParser(object):
 
     def parse_file(self, file_extension, input_stream):
         if file_extension == Language.Java.value:
-            return LanguageParser.parse_java9_file(self, input_stream)
+            return LanguageParser.parse_java_file(self, input_stream)
         elif file_extension == Language.Kotlin.value:
             return LanguageParser.parse_kotlin_file(self, input_stream)
 
