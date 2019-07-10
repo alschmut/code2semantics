5cab0a2f939749d97401f240b61523d8689f9387
alschmut <aschmutz1@web.de>
2019-07-10 12:14:53 +0100

    Refactor ParseIdentifier main()

--- src/main/ParseIdentifier.py
+++ src/main/ParseIdentifier.py
@@ -28,10 +28,6 @@ def get_time_duration(start):
 def print_finished(file_path, start):
 	print(f"\r[+] Finished ({get_time_duration(start)}s): {file_path}")
 
-
-
-
-
 def parse_file_if_supported(file_path):
 	start = time.time()
 	file_extension = get_file_extension(file_path)
@@ -63,8 +59,7 @@ def parse_file(file_path):
 	parsed_file[file_path] = parse_file_if_supported(file_path)
 	return parsed_file
 
-def get_path_without_trailing_slash():
-	path = sys.argv[1]
+def get_path_without_trailing_slash(path):
 	if path[-1] == "/":
 		path = path[:-1]
 	return path
@@ -74,20 +69,26 @@ def save_file(parser_data):
 	with open(output_file_name, 'w') as f:
 		f.write(str(parser_data))
 
-def parse(path):
-	if os.path.isdir(path):
-		save_file(traverse_directory(path))
-	else:
-		save_file(parse_file(path))
-
 def main():
 	if len(sys.argv) != 2:
 		print(f'[-] Usage: python {sys.argv[0]} <file_or_directory_path>')
-		test_dir = "/Users/alexandersch/Documents/Beruf/Master/7 Thesis/Antlr/test_directory/subdir1/subdir2"
-		print(f'[-] Use default test directory "{test_dir}"')
-		sys.argv.append(test_dir)
+		return
 
-	parse(get_path_without_trailing_slash())
+	path = get_path_without_trailing_slash(sys.argv[1])
+	start = time.time()
+
+	if os.path.isfile(path) or os.path.isdir(path):
+		print(f'[+] Parse file "{path}"')
+		save_file(parse_file(path))
+		print(f"[+] Finished: {get_time_duration(start)} seconds")
+
+	elif os.path.isdir(path):
+		print(f'[+] Parse all supported files in directory "{path}"')
+		save_file(traverse_directory(path))
+		print(f"[+] Finished: {get_time_duration(start)} seconds")
+
+	else: 
+		print(f'[-] Could not find file or directory: "{path}"')
 
 if __name__ == '__main__':
     main()
