f38820fb5de155ac85b28aa97944ab57c079357e
alschmut <aschmutz1@web.de>
2019-07-10 17:56:46 +0100

    Add SemanticsCalculator functionality

--- src/main/SemanticsCalculator.py
+++ src/main/SemanticsCalculator.py
@@ -1,18 +1,46 @@
 import multiprocessing
+import sys, os, time, json
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
-import sys, os, time
+from FileOpener import FileOpener
+from ProjectModel import ProjectModel
+
+def get_avg_distance(model: str, word_list: [str]):
+	most_similar_word_pair = model.wv.most_similar(positive=word_list, topn=1)
+	print(most_similar_word_pair)
+	centric_word = most_similar_word_pair[0][0]
+	distances_to_centric_vector = model.wv.distances(centric_word, word_list)
+	distance = 0
+	for word_tuple in distances_to_centric_vector:
+		distance += word_tuple
+	return distance / len(word_list)
+
+def get_word_list(model, file):
+	word_list = []
+	idewntifiers = file.get("identifiers")
+	for type in idewntifiers:
+		for word_obj in idewntifiers.get(type):
+			filtered_words = [word for word in word_obj.get("partial") if model.wv.vocab.get(word) != None]
+			word_list.extend(filtered_words)
+	return word_list
 
 def get_output_file(file_path: str):
 	file_name = file_path.split("/")[-1]
 	language_code = file_name.split(".")[1]
 	output_file_name = f"wiki.{language_code}.semantic_distance.csv"
-	return open(output_file_name, 'wb')
+	return open(output_file_name, 'w')
 
-def calculate_semantic_distance(file_path: str, parser_path: str):
-	model = Word2Vec.load(file_path)
-	word_list = []
-	model.most_similar(positive=word_list, topn=1)
+def calculate_semantic_distance(model_file_path: str, project_file_path: str):
+	model: Word2Vec = Word2Vec.load(model_file_path)
+	project: ProjectModel = json.loads(FileOpener().get_file_content(project_file_path))
+	output_file = get_output_file(model_file_path)
+	output_file.writelines("path,semantic_distance\n")
+
+	for file in project:
+		word_list: [str] = get_word_list(model, file)
+		print(file.get("name"))
+		avg_distance = get_avg_distance(model, word_list)
+		output_file.writelines(file.get("name") + "," + str(avg_distance) + "\n")
 
 def get_time_duration(start: float):
 	end: float = time.time()
@@ -20,21 +48,21 @@ def get_time_duration(start: float):
 
 def main():
 	if len(sys.argv) != 3:
-		print(f'[-] Usage: python {sys.argv[0]} <word2vec.model> <parser_data.json>')
+		print(f'[-] Usage: python {sys.argv[0]} <word2vec.model> <project_data.json>')
 		return
 
 	model_file_path = sys.argv[1]
-	parser_file_path = sys.argv[2]
+	project_file_path = sys.argv[2]
 
-	if os.path.isfile(model_file_path) and os.path.isfile(parser_file_path):
+	if os.path.isfile(model_file_path) and os.path.isfile(project_file_path):
 		print(f'[+] Use model "{model_file_path}"')
-		print(f'[+] Use parser_data "{parser_file_path}"')
+		print(f'[+] Use project_data "{project_file_path}"')
 		print("[+] Starting to calculate semantic distance")
 		start: float = time.time()
-		calculate_semantic_distance(model_file_path, parser_file_path)
-		print(f"[+] Finished: {get_time_duration(start)} seconds")
+		calculate_semantic_distance(model_file_path, project_file_path)
+		print(f"[+] Finished: {get_time_duration(start)}s")
 	else:
-		print(f"[-] Could not find file: {parser_file_path}")
+		print(f"[-] Could not find file: {project_file_path}")
 
 if __name__ == '__main__':
     main()
