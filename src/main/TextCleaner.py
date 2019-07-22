cd2af70521d34efc9389a19617c28a7141833a3a
alschmut <aschmutz1@web.de>
2019-07-22 15:27:17 +0100

    Fix removing stopwords in TextCleaner

--- src/main/TextCleaner.py
+++ src/main/TextCleaner.py
@@ -12,12 +12,12 @@ def print_status(num_articles: int):
 def remove_stopwords_lemmatize_wiki(file_path: str):
 	sp = spacy.load("en")
 	stop_words = set(stopwords.words('english'))
-	output_file = FileOpener().get_new_file("wiki.en.lemmatised.text", "a")
+	output_file = FileOpener().get_new_file("wiki.en.lemmatised.txt", "a")
 	with open(file_path, "r") as file:
 		i = 0
 		for line in file:
 			spacy_line = sp(line)
-			lemmatized_list = [word.lemma_ for word in spacy_line if word not in stop_words]
+			lemmatized_list = [word.lemma_ for word in spacy_line if word.text not in stop_words]
 			lemmazized_line = " ".join(lemmatized_list)
 			output_file.write(lemmazized_line)
 			i = i + 1
@@ -34,7 +34,7 @@ def main():
 
 	if os.path.isfile(file_path):
 		print(f'[+] Use raw text "{file_path}"')
-		print("[+] Starting to lemmatize text")
+		print("[+] Starting to remove stopwords and lemmatize text")
 		timer = Timer()
 		remove_stopwords_lemmatize_wiki(file_path)
 		print(f"\n[+] Finished: {timer.get_duration()}s")
