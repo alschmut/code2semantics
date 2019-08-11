45a5b0f8cced1f324248e930d4831596f63e9e25
alschmut <aschmutz1@web.de>
2019-08-11 16:55:37 +0200

    Update survey structure step 1

--- src/main/evaluation/FileModel.py
+++ src/main/evaluation/FileModel.py
@@ -1,26 +1,29 @@
 from evaluation.IdentifierModel import IdentifierModel
 from evaluation.FileContext import FileContext
 from evaluation.ContextIdentifier import ContextIdentifier
+from model.IdentifierType import IdentifierType
 
 class FileModel():
 	semantic_context_identifier = None
-	file_context = None
-	class_names = None
-	method_names = None
-	variable_names = None
+	class_name_describes_context = None
+	identifier_list: [] = None
 
 	def __init__(self, file):
 		self.semantic_context_identifier = ContextIdentifier(file.get("semantic_context_identifier"))
-		self.file_context = FileContext(file.get("file_context"))
-		self.class_names = {key: IdentifierModel(value) for (key, value) in file.get("class_names").items()}
-		self.method_names = {key: IdentifierModel(value) for (key, value) in file.get("method_names").items()}
-		self.variable_names = {key: IdentifierModel(value) for (key, value) in file.get("variable_names").items()}
+		self.class_name_describes_context = file.get("file_context").get("numeric")
+		self.identifier_list = []
+		for (key, value) in file.get("class_names").items():
+			self.identifier_list.append(IdentifierModel(key, value, IdentifierType.Class))
+
+		for (key, value) in file.get("method_names").items():
+			self.identifier_list.append(IdentifierModel(key, value, IdentifierType.Method))
+
+		for (key, value) in file.get("variable_names").items():
+			self.identifier_list.append(IdentifierModel(key, value, IdentifierType.Variable))
 
 	def to_print(self):
 		return {
 			"semantic_context_identifier": self.semantic_context_identifier.to_print(),
-			"file_context": self.file_context.to_print(),
-			"class_names": {key: value.to_print() for (key, value) in self.class_names.items()},
-			"method_names": {key: value.to_print() for (key, value) in self.method_names.items()},
-			"variable_names": {key: value.to_print() for (key, value) in self.variable_names.items()}
+			"class_name_describes_context": self.class_name_describes_context,
+			"identifier_list": [identifier.to_print() for identifier in self.identifier_list],
 		}
--- src/main/evaluation/IdentifierModel.py
+++ src/main/evaluation/IdentifierModel.py
@@ -1,14 +1,18 @@
+from model.IdentifierType import IdentifierType
 
 class IdentifierModel():
+	name: str = None
 	usefulness: str = None
-	numeric: str = None
+	type: IdentifierType = None
 
-	def __init__(self, identifier):
-		self.numeric = identifier.get("numeric")
-		self.usefulness = identifier.get("usefulness")
+	def __init__(self, name, identifier, type: IdentifierType):
+		self.name = name
+		self.usefulness = identifier.get("numeric")
+		self.type = type
 
 	def to_print(self):
 		return {
-			"usefulness": self.usefulness,
-			"numeric": self.numeric
+			"name": self.name,
+			"type": self.type.value,
+			"usefulness": self.usefulness
 		}
