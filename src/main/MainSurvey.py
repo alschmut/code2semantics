5fd309516a17380375e4545d09bebea6342cbcb3
alschmut <aschmutz1@web.de>
2019-08-12 16:21:04 +0200

    Remove evaluation folder

--- src/main/MainSurvey.py
+++
@@ -1,9 +0,0 @@
-import json
-from evaluation.SurveyModel import SurveyModel
-from util.FileOpener import FileOpener
-
-def main():
-	FileOpener().save_file_as_json(SurveyModel().to_print(), "survey.json")
-
-if __name__ == "__main__":
-	main()
